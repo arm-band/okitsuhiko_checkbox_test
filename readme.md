c906576d2b5c4c73d482ee6e3c03af1f53f1feac
arm-band <bre4tn.let@gmail.com>
2020-11-09 23:21:15 +0900

    first commit.

--- readme.md
+++ readme.md
@@ -14,3 +14,8 @@ gulpを使用した最低限のフレームワークです。
 
 〽酒は呑め呑め　呑むならば　日本一の此の槍を  
 　呑み取る程に　呑むならば　是ぞ真の黒田武士
+
+## Reference
+
+- [jQueryでチェックボックスの value を複数取得するやり方 \| PisukeCode \- Web開発まとめ](https://pisuke-code.com/jquery-get-checked-vals-as-array/)
+- [JavaScript｜「含むか」の判定\(配列, 文字列\) \- わくわくBank](https://www.wakuwakubank.com/posts/485-javascript-include-array-string/)
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,40 @@
-window.addEventListener('load', () => {
-    const helloMori = 'hello Mori!';
-
-    console.log(helloMori);
-
-    const sHelloMori = document.querySelector('#hello');
-    if(sHelloMori !== undefined && sHelloMori !== null) {
-        sHelloMori.textContent = helloMori;
+$(() => {
+    const input = 'input[name="ichimatsu[]"]';
+    const Breathing = 'Breathing';
+    const Sunshine = 'Sunshine';
+    const Pillar = 'Pillar';
+    const $Weakpoint = $(`#Weakpoint`);
+    const $Pillar = $(`#${Pillar}`);
+    // initialize
+    if (!$(input).prop('checked')) {
+        $Weakpoint.attr('readonly', 'readonly');
+        $Pillar.attr('readonly', 'readonly');
     }
+    // click event
+    $(input).on('change', function () {
+        if ($(this).prop('checked')) {
+            const checkedVal = $(this).val();
+            if (checkedVal === Breathing || checkedVal === Sunshine) {
+                $Weakpoint.removeAttr('readonly');
+                $Weakpoint.trigger('focus');
+            }
+            else if (checkedVal === 'Pillar') {
+                $Pillar.removeAttr('readonly');
+                $Pillar.trigger('focus');
+            }
+        }
+        else {
+            const checks = $(`${input}:checked`).map( function () {
+                return $(this).val();
+            }).get();
+            if (!checks.includes('Breathing') && !checks.includes('Sunshine')) {
+                $Weakpoint.attr('readonly', 'readonly');
+                $Weakpoint.trigger('blur');
+            }
+            if (!checks.includes('Pillar')) {
+                $Pillar.attr('readonly', 'readonly');
+                $Pillar.trigger('blur');
+            }
+        }
+    });
 });
